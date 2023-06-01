4de5b245bc985e9a58a1da48d5844ee4a2e368db
Alma Alvarado <47694944+almaavocado@users.noreply.github.com>
2023-06-01 14:20:39 -0700

    added narvar

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import "./navbar.css";
+
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+      <ul className="navbar-list">
+        <li className="navbar-item"><a href="/" className="navbar-link">Home</a></li>
+        <li className="navbar-item"><a href="/about" className="navbar-link">About</a></li>
+        <li className="navbar-item"><a href="/services" className="navbar-link">Services</a></li>
+        <li className="navbar-item"><a href="/contact" className="navbar-link">Contact</a></li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
